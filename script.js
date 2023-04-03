5cfc0bfb5767ef4d99e6baa3886f2a44e2c90d77
kayo jose <kayojfsilva@fac.pe.senac.br>
2023-04-03 11:23:25 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  if (username === "" || password === "") {
+    alert("Por favor, preencha todos os campos.");
+  } else if (username === "usuario" && password === "senha") {
+    alert("Bem-vindo!");
+    window.location.href = "pagina-secreta.html";
+  } else {
+    alert("Nome de usuário ou senha incorretos.");
+  }
+}
